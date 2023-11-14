4c3dc0b3a73978cde9dd7c28df336c0005b59c1f
ayobamy <ayobamyahmed@gmail.com>
2023-11-14 04:09:20 +0100

    (update): user, auth, and expense module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,46 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ExpensesModule } from './expenses/expenses.module';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [AuthModule, UsersModule, ExpensesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+      }),
+      isGlobal: true,
+      load: [configuration],
+      cache: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<TypeOrmModuleOptions> => ({
+        type: 'postgres',
+        host: configService.get<string>('database.dev.host'),
+        port: configService.get<number>('database.dev.port'),
+        username: configService.get<string>('database.dev.username'),
+        password: configService.get<string>('database.dev.password'),
+        database: configService.get<string>('database.dev.db'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    AuthModule,
+    UsersModule,
+    ExpensesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
