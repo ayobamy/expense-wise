6048749d9c7f997fa23c4d6d625c9b230b0aacf3
ayobamy <ayobamyahmed@gmail.com>
2023-11-14 04:07:09 +0100

    feat(setup): setup expense entity file

--- src/expenses/entities/expense.entity.ts
+++ src/expenses/entities/expense.entity.ts
@@ -1,1 +1,38 @@
-export class Expense {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Expense {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  amount: number;
+
+  @Column({ type: 'date' })
+  date: string;
+
+  @Column()
+  category: string;
+
+  @ManyToOne(() => User, (user) => user.expenses)
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date;
+}
